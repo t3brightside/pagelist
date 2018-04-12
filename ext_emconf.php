a6594e055644a8bbd7d0e05115221c6079d4b8c6
Tanel Põld <tanel@brightside.ee>
2018-04-12 12:51:35 +0800

    Compatibility version

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' =>
 		array (
-			'typo3' => '8.7.0 - 9.99.99',
+			'typo3' => '8.7.0 - 9.9.99',
 			'fluid_styled_content' => '',
 		),
 		'conflicts' =>
