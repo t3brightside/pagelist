6c8b27ae0d8cbcd5f2112401f9a9b721f6931c10
Tanel Põld <tanel@brightside.ee>
2018-07-20 12:27:27 +0300

    Cleanup

--- Configuration/PageTS/setup.ts
+++ Configuration/PageTS/setup.ts
@@ -1,27 +1,3 @@
-#TCEFORM {
-#	pages {
-#	    title.label = Article title
-#	    subtitle.label = Sub note (event date, location etc...)
-#	    abstract.label = Teaser (showin in news list)
-
-#		tx_realurl_pathoverride.disabled = 1
-#		tx_advancedtitle_absolute.disabled = 1
-#		tx_advancedtitle_prefix.disabled = 1
-#		tx_advancedtitle_sufix.disabled = 1
-#		content_from_pid.disabled = 1
-#		target.disabled = 0
-#		cache_timeout.disabled = 1
-#		cache_tags.disabled = 1
-#		no_cache.disabled = 1
-#		is_siteroot.disabled = 1
-#		no_search.disabled = 0
-#		php_tree_stop.disabled = 1
-#		module.disabled = 1
-#		categories.disabled = 0
-#		media.disabled = 0
-#		tx_metaplus_favicon.disabled = 1
-#	}
-#}
 
 TCAdefaults.pages {
 	tx_pagelist_datetime = now
