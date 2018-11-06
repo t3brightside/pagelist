44e6790795e153d065bcf426a14a525b066178c3
Tanel Põld <tanel@brightside.ee>
2018-11-06 11:29:44 +0800

    Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,5 @@
 <?php
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Pagelist',
 	'description' => 'Create page lists, teasers and add page types for news, events and products.',
 	'category' => 'fe',
@@ -11,12 +11,16 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Tanel Põld',
 	'author_email' => 'tanel@brightside.ee',
 	'author_company' => 'Brightside OÜ / t3brightside.com',
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
+	'constraints' => [
+		'depends' => [
 			'typo3' => '8.7.0 - 9.5.99',
 			'fluid_styled_content' => '',
-		),
-	),
-);
+		],
+		'conflicts' => [
+			'blog' => '',
+		],
+    'suggests' => [
+			'personnel' => '',
+		],
+	],
+];
