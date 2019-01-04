1b1399822b00991ff1ed76230a44a80b92899353
Tanel Põld <tanel@brightside.ee>
2019-01-04 14:05:45 +0800

    Check if page list is loaded and add TS constant

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,3 +6,13 @@ defined('TYPO3_MODE') || die('Access denied.');
 	'Configuration/TypoScript/',
 	'Pagelist'
 );
+
+if (TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('personnel')){
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+		pagelist.personnelIsLoaded = 1
+	');
+} else {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('
+		pagelist.personnelIsLoaded = 0
+	');
+}
