1ef84a950e829ade335343ef3f67fe7b43d18a71
Tanel Põld <tanel@brightside.ee>
2021-11-09 14:12:49 +0200

    autoload fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 		'depends' => [
 			'typo3' => '11.5.0 - 11.5.99',
 			'fluid_styled_content' => '11.5.0 - 11.5.99',
-			'paginatedprocessors' => '1.0.0 - 1.0.99',
+			'paginatedprocessors' => '1.0.0 - 1.99.99',
 		],
 		'conflicts' => [
 			'blog' => '',
@@ -21,8 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'suggests' => [
 			'personnel' => '',
 		],
-		'autoload' => [
-        'classmap' => ['Classes'],
-    ],
+	],
+	'autoload' => [
+		 'psr-4' => [
+				'Brightside\\Pagelist\\' => 'Classes'
+		 ]
 	],
 ];
