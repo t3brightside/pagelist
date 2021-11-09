5f6f5f4a68cb4c67ff12b113cbadfdd900ccbf44
Tanel Põld <tanel@brightside.ee>
2021-11-09 16:13:38 +0200

    paginatedprocessors min 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 		'depends' => [
 			'typo3' => '11.5.0 - 11.5.99',
 			'fluid_styled_content' => '11.5.0 - 11.5.99',
-			'paginatedprocessors' => '1.0.0 - 1.99.99',
+			'paginatedprocessors' => '1.0.1 - 1.99.99',
 		],
 		'conflicts' => [
 			'blog' => '',
