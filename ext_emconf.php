dd044256ccbf3fbb06d114e79d76e60469d6ce00
Tanel Põld <tanel@brightside.ee>
2023-01-16 00:58:45 +0200

    [TASK] Release 3.5.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Pagelist',
     'description' => 'Create page lists/teasers and add page types for news, events, products and vacancies.',
     'category' => 'fe',
-    'version' => '3.5.2',
+    'version' => '3.5.3',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
     'author' => 'Tanel Põld',
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '11.5.0 - 12.99.99',
             'fluid_styled_content' => '11.5.0 - 12.99.99',
-            'paginatedprocessors' => '1.4.0 - 1.99.99',
+            'paginatedprocessors' => '1.4.2 - 1.99.99',
         ],
         'conflicts' => [
             'blog' => '',
