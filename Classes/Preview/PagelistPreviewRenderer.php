0289d854f1f74a0095984105226f4615595b1459
Tanel Põld <tanel@brightside.ee>
2024-06-14 11:12:19 +0300

    [TASK] imporved back end preview

--- Classes/Preview/PagelistPreviewRenderer.php
+++ Classes/Preview/PagelistPreviewRenderer.php
@@ -18,78 +18,83 @@ declare(strict_types=1);
 namespace Brightside\Pagelist\Preview;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
-use TYPO3\CMS\Backend\Preview\PreviewRendererInterface;
-use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperRegistry;
-/**
- * Contains a preview rendering for the page module of CType="textmedia"
- * @internal this is a concrete TYPO3 hook implementation and solely used for EXT:frontend and not part of TYPO3's Core API.
- */
+use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Core\Domain\Repository\PageRepository;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\MathUtility;
+
 class PagelistPreviewRenderer extends StandardContentPreviewRenderer
 {
     public function renderPageModulePreviewContent(GridColumnItem $item): string
     {
-        $content = '';
-        $row = $item->getRecord();
-        if ($row['CType'] === 'pagelist_selected' || $row['CType'] === 'pagelist_sub' || $row['CType'] === 'pagelist_category') {
-            if ($row['CType'] === 'pagelist_selected') {
-                $content = $this->linkEditContent('<span style="display: block; margin-top: 0.3em;">Pagelist: selected '. $row['pages'] .'</span>', $row);
-            }
-            if ($row['CType'] === 'pagelist_sub') {
-                $content = $this->linkEditContent('<span style="display: block; margin-top: 0.3em;">Pagelist: subpages from '. $row['pages'] .'</span>', $row);
-            }
-            if ($row['CType'] === 'pagelist_category') {
-                $content = $this->linkEditContent('<span style="display: block; margin-top: 0.3em;">Pagelist: from category '. $row['selected_categories'] .'</span>', $row);
-            }
-            $content .= '<ul style="margin: 0; padding: 0.2em 1.4em;">';
-            if ($row['tx_pagelist_template']) {
-                $content .= '<li>' . $this->linkEditContent($this->renderText('Template: ' . $row['tx_pagelist_template']), $row) . '</li>';
-            }
-            if ($row['tx_pagelist_titlewrap']) {
-                $content .= '<li>' . $this->linkEditContent($this->renderText('Title tag: ' . $row['tx_pagelist_titlewrap']), $row) . '</li>';
-            }
-            if ($row['CType'] === 'pagelist_sub') {
-                if ($row['tx_pagelist_orderby']) {
-                    $content .= '<li>' . $this->linkEditContent($this->renderText('Order by: ' . $row['tx_pagelist_orderby']), $row) . '</li>';
-                }
-            }
-            if ($row['CType'] === 'pagelist_sub' || $row['CType'] === 'pagelist_category') {
-                if ($row['tx_pagelist_startfrom']) {
-                    $content .= '<li>' . $this->linkEditContent($this->renderText('Start from: ' . $row['tx_pagelist_startfrom']), $row) . '</li>';
-                }
-            }
-            if ($row['CType'] === 'pagelist_sub' || $row['CType'] === 'pagelist_category') {
-                if ($row['tx_pagelist_limit']) {
-                    $content .= '<li>' . $this->linkEditContent($this->renderText('Items shown: ' . $row['tx_pagelist_limit']), $row) . '</li>';
-                }
-            }
-            if ($row['tx_pagelist_disableimages'] == 1) {
-                $content .= '<li>' . $this->linkEditContent('Images: disabled', $row) . '</li>';
-            }
-            if ($row['tx_pagelist_disableabstract'] == 1) {
-                $content .= '<li>' . $this->linkEditContent('Introduction: disabled', $row) . '</li>';
-            }
-            if ($row['tx_pagelist_authors'] > 0) {
-                $content .= '<li>' . $this->linkEditContent('Author filter: active', $row) . '</li>';
-            }
-            if ($row['tx_paginatedprocessors_paginationenabled'] == 1) {
-                $content .= '<li>' . $this->linkEditContent('Pagination: enabled', $row) . '</li>';
-            }
-            if ($row['tx_paginatedprocessors_itemsperpage'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
-                $content .= '<li>' . $this->linkEditContent($this->renderText('Items per page: ' . $row['tx_paginatedprocessors_itemsperpage']), $row) . '</li>';
-            }
-            if ($row['tx_paginatedprocessors_pagelinksshown'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
-                $content .= '<li>' . $this->linkEditContent($this->renderText('Links shown: ' . $row['tx_paginatedprocessors_pagelinksshown']), $row) . '</li>';
-            }
-            if ($row['tx_paginatedprocessors_urlsegment'] && $row['tx_paginatedprocessors_paginationenabled'] == 1) {
-                $content .= '<li>' . $this->linkEditContent($this->renderText('URL segment: ' . $row['tx_paginatedprocessors_urlsegment']), $row) . '</li>';
-            }
-            $content .= '</ul>';
-        }
-        return $content;
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(
+            'EXT:pagelist/Resources/Private/Templates/Backend/Preview.html',
+        );
 
+        $record = $item->getRecord();
+        $view->assign('pagelistitem', $record);
+
+        // Initialize an array to store pagelist records
+        $pagelistRecords = [];
+
+        $CType = $record['CType'];
+        $pids = $record['pages'];
+
+        $selectedCategories = $record['selected_categories'];
+        $authorUids = $record['tx_pagelist_authors'];
+
+        // Get page titles
+        if ($pids) {
+            $pageIds = explode(',', $pids);
+            $pageIds = array_map('intval', $pageIds);
+            $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
+            $pageTitles = [];
+            foreach ($pageIds as $pageUid) {
+                $pageData = $pageRepository->getPage($pageUid);
+                if ($pageData && isset($pageData['title'])) {
+                    $pageTitles[] = $pageData['title'];
+                }
+            }
+            $view->assign('pageTitles', $pageTitles);
+        }
+
+        // Show filtering selections
+        if ($CType == 'pagelist_sub') {
+
+            // Get selected catefories
+            if ($selectedCategories) {
+                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+                $query = $queryBuilder
+                    ->select('title')
+                    ->from('sys_category')
+                    ->where(
+                        $queryBuilder->expr()->in('uid', $selectedCategories)
+                    );
+                $queryResult = $query->executeQuery();
+                $cetegoryTitles = $queryResult->fetchAllAssociative();
+                $view->assign('catTitles', $cetegoryTitles);
+            }
+
+            // Get selected authors
+            if ($authorUids) {
+                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_personnel_domain_model_person');
+                $query = $queryBuilder
+                    ->select('*')
+                    ->from('tx_personnel_domain_model_person')
+                    ->where(
+                        $queryBuilder->expr()->in('uid', $authorUids)
+                    );
+                $queryResult = $query->executeQuery();
+                $authors = $queryResult->fetchAllAssociative();
+                $view->assign('authors', $authors);
+            }
+        }
+
+        $out = $view->render();
+        return $this->linkEditContent($out, $record);
     }
 }
