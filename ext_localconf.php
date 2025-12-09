4b4f629773d4ccca2fb473134576f08ee24a6c8a
Tanel Põld <tanel@brightside.ee>
2025-12-09 10:17:09 +0200

    [TASK] remove deprecations and clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,10 @@
 <?php
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die('Access denied.');
 
-(function () {
-    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-    // Only include wizard.tsconfig if TYPO3 version is below 13
-    if ($versionInformation->getMajorVersion() < 13) {
-        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-            @import "EXT:pagelist/Configuration/TSConfig/wizard.tsconfig"
-        ');
-    }
-
-    
+(function () {    
     if (ExtensionManagementUtility::isLoaded('personnel')) {
         ExtensionManagementUtility::addTypoScriptConstants('
             pagelist.personnelIsLoaded = 1
@@ -27,10 +16,12 @@ defined('TYPO3') || die('Access denied.');
     }
 
     ExtensionUtility::configurePlugin(
-        'Pagelist',
+        'pagelist',
         'Pagelist',
         [
             'Pagelist' => 'pagelist',
-        ]
+        ],
+        [], 
+        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT 
     );
 })();
